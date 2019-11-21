927f0e86a40e01af16a96549d5c6ae36243c7443
Priti <priti@gmail.com>
2019-11-21 19:53:46 +0530

    arithmatic function added

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"arith.h"
+int main()
+{
+    printf("addition = %d\n",add(10,20));
+    printf("sub = %d\n",sub(20,10));
+    printf("mul = %d\n",mul(20,10));
+    printf("div = %d\n",div(20,10));
+    return 0;
+}
